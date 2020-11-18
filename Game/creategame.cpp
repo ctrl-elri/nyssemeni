4efa44fcf5f1f1f70cd458ebfda761ef81068522
Ella Rinnemaa <ella.rinnemaa@tuni.fi>
2020-11-18 09:02:29 +0200

    Modified shared_ptr in creategame

--- Game/creategame.cpp
+++ Game/creategame.cpp
@@ -2,7 +2,7 @@
 #include "city.hh"
 #include <algorithm>
 
-#include <string>
+#include <memory>
 
 QImage DEFAULT_MAP_PIC(":/offlinedata/offlinedata/kartta_pieni_500x500.png");
 QImage BIG_MAP_PIC(":/offlinedata/offlinedata/kartta_iso_1095x592.png");
@@ -10,11 +10,10 @@ QImage BIG_MAP_PIC(":/offlinedata/offlinedata/kartta_iso_1095x592.png");
 
 std::shared_ptr<Interface::ICity> Interface::createGame()
 {
-    Interface::ICity* newCity = new City;
+    City* newCity = new City;
     newCity->setBackground(DEFAULT_MAP_PIC, BIG_MAP_PIC);
 
-    std::shared_ptr<Interface::ICity> city(nullptr);
-    city = std::make_shared<Interface::ICity>(newCity);
+    std::shared_ptr<Interface::ICity> city = std::make_shared<Interface::ICity>(newCity);
 
     return city;
     
