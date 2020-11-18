94373b3049bbfad25866968963ac1d2a03db5ddd
Heidi Seppi <seppi@linux-desktop5.tuni.fi>
2020-11-18 08:51:18 +0200

    Modified city.cpp and creategame.cpp

--- Game/city.cpp
+++ Game/city.cpp
@@ -1,6 +1,6 @@
 #include "city.hh"
 
-City::City():Interface::ICity()
+City::City()//:Interface::ICity()
 {
     
 }
@@ -22,12 +22,50 @@ void City::setClock(QTime clock)
    }
 }
 
+void City::addStop(std::shared_ptr<Interface::IStop> stop)
+{
+
+}
+
 void City::startGame()
 {
 
 }
 
+void City::addActor(std::shared_ptr<Interface::IActor> newactor)
+{
 
+}
+
+void City::removeActor(std::shared_ptr<Interface::IActor> actor)
+{
+
+}
+
+void City::actorRemoved(std::shared_ptr<Interface::IActor> actor)
+{
+
+}
+
+bool City::findActor(std::shared_ptr<Interface::IActor> actor) const
+{
+
+}
+
+void City::actorMoved(std::shared_ptr<Interface::IActor> actor)
+{
+
+}
+
+std::vector<std::shared_ptr<Interface::IActor> > City::getNearbyActors(Interface::Location loc) const
+{
+
+}
+
+bool City::isGameOver() const
+{
+
+}
 
 
 
--- Game/creategame.cpp
+++ Game/creategame.cpp
@@ -1,7 +1,7 @@
 #include "creategame.hh"
 #include "city.hh"
 #include <algorithm>
-#include <iostream>
+
 #include <string>
 
 QImage DEFAULT_MAP_PIC(":/offlinedata/offlinedata/kartta_pieni_500x500.png");
@@ -10,12 +10,12 @@ QImage BIG_MAP_PIC(":/offlinedata/offlinedata/kartta_iso_1095x592.png");
 
 std::shared_ptr<Interface::ICity> Interface::createGame()
 {
-    //::ICity *newCity = std::getObject(true);
-    Interface::ICity *city = static_cast<City*>();
-    city->setBackground(DEFAULT_MAP_PIC, BIG_MAP_PIC);
+    Interface::ICity* newCity = new City;
+    newCity->setBackground(DEFAULT_MAP_PIC, BIG_MAP_PIC);
 
-    std::shared_ptr<Interface::ICity> c(city);
+    std::shared_ptr<Interface::ICity> city(nullptr);
+    city = std::make_shared<Interface::ICity>(newCity);
 
-    //return city;
+    return city;
     
 }
