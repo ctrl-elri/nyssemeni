d38ce3933336468253421053d236e0aa0259936d
Ella Rinnemaa <ella.rinnemaa@tuni.fi>
2020-11-22 14:16:36 +0200

    Added commentary to mainwindow's methods

--- Game/mainwindow.hh
+++ Game/mainwindow.hh
@@ -25,18 +25,47 @@ public:
 
     void addActor(int locX, int locY, int type);
 
+    /**
+     * @brief setPicture sets the background picture.
+     * @param img background picture, from City.
+     */
     void setPicture(QImage &img);
 
+    /**
+     * @brief moveActorItem moves actor's actoritem in window.
+     * @param actorToBeMoved actor whose actoritem is to be moved, from City.
+     */
     void moveActorItem(std::shared_ptr<Interface::IActor> actorToBeMoved);
 
+    /**
+     * @brief getActor sets latest added actor as lastAc_.
+     * @param newac latest added actor, from City.
+     */
     void getActor(std::shared_ptr<Interface::IActor> newac);
 
+    /**
+     * @brief addToMap adds a pair to map, actors_, the pair consisting of
+     * key; lastAc_, and value; *lastItem_.
+     */
     void addToMap();
 
+    /**
+     * @brief removeActorItem removes an element from map, actors_.
+     * @param actorToBeRemoved key to the element which is to be removed, from City.
+     */
     void removeActorItem(std::shared_ptr<Interface::IActor> actorToBeRemoved);
 
+    /**
+     * @brief addPlayer adds a playeritem to the window and to the vector players_.
+     * @param locX starting position's x-coordinate.
+     * @param locY starting position's y-coordinate.
+     * @param type item's type.
+     */
     void addPlayer(int locX, int locY, int type);
 
+    /**
+     * @brief checkPlayerMovement checks that the player moves within window.
+     */
     void checkPlayerMovement();
 
 private slots:
